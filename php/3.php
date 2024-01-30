21c5e01b9b18db8ceba981483fe16bb072abd824
Mochamad Ridwan <106060496+mchridwan04@users.noreply.github.com>
2024-01-31 01:11:34 +0700

    day 1

---
+++ php/3.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0
+*/
+
+function positive_sum($arr)
+{
+      $result = 0;
+      foreach ($arr as $a) {
+            if ($a > 0) {
+                  $result += $a;
+            }
+      }
+      return $result;
+}
+
+echo positive_sum([1, -4, 7, 12]);
